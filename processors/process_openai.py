450de366867552e7c18506199901fe00f7dbaf3d
Rajesh Ghosh <rajesh_ghosh@persistent.com>
2024-01-11 20:30:36 +0530

    lambda fix

--- processors/process_openai.py
+++ processors/process_openai.py
@@ -96,7 +96,7 @@ def call_openai(LLM_choice, user_name_logged, prompt, messages, model_name, max_
     PROMPT_INSERT_LAMBDA = config.PROMPT_INSERT_LAMBDA
     lambda_function_name = PROMPT_INSERT_LAMBDA
     (lambda_response_code, lambda_response) = invoke_lambda (lambda_function_name, data)
-    if lambda_response != 200:
+    if lambda_response_code != 200:
         raise Exception(f"AWS Lambda invocation failed with status code: {lambda_response['StatusCode']}")
     else:
         print ("Success calling lambda!")        
