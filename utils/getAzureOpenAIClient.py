f5d181d983ecbc48e63c571fd62cc2f10c43707c
Avinash Tiwari <avinash_tiwari2@persistent.com>
2024-01-12 10:15:21 +0530

    Refactor Code for getAzureOpenAIClient.py

--- utils/getAzureOpenAIClient.py
+++ utils/getAzureOpenAIClient.py
@@ -1,7 +1,7 @@
 import os
 import dotenv 
-from openai import OpenAI
-import config as config
+from openai import OpenAI,AzureOpenAI
+import config as config 
 
 def get_openai_azure_core_client():    
     print('get_openai_azure_core_client')
@@ -11,7 +11,7 @@ def get_openai_azure_core_client():
     for key in env_vars:
         os.environ[key] = env_vars[key]
     
-    from openai import AzureOpenAI
+    
     
     client = AzureOpenAI(
         api_key=config.AZURE_OPENAI_API_KEY,  
